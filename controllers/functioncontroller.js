22c11f917a964d2eef3417a47841e60cd2e458b4
Suresh Muruganantham <sureshsanjaymail@gmail.com>
2022-08-31 18:16:57 +0530

    contact api integrated

--- controllers/functioncontroller.js
+++ controllers/functioncontroller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt-nodejs');
 const fs = require('fs');
+const blogs = require('../models/blogs');
 const Blogs = require('../models').blogs;
+const Contact = require('../models').contact;
 
 module.exports.createBlogs = function (req) {
     let blogs = req.body ;
@@ -14,7 +16,7 @@ module.exports.createBlogs = function (req) {
             title: blogs.title,
             description: blogs.description,
             image: img_url,
-            category:blogs.category
+            category:blogs.category.toLowerCase()
         })
         .then((blogsResult) => {
             blogsResult = blogsResult && typeof blogsResult == 'object' ? blogsResult.toJSON() : blogsResult;
@@ -52,6 +54,61 @@ module.exports.getBlogs = function (category) {
     console.log("Data ",category )
 
     return Blogs
+    .findAll({where: {category: category}})
+    .then(blogResponse => {
+        return {
+            message:"success",
+            data:blogResponse
+        } ;
+    }).catch((error) => {
+        return {
+            message:"something went wrong",
+        }
+    });
+};
+
+
+module.exports.createContact = function (blogs) {
+    return Contact
+        .create({
+            name: blogs.name,
+            contact_no: blogs.contact_no,
+            email: blogs.email,
+            category:blogs.category.toLowerCase()
+        })
+        .then((blogsResult) => {
+            blogsResult = blogsResult && typeof blogsResult == 'object' ? blogsResult.toJSON() : blogsResult;
+            const data = new Object();
+            for (const prop in blogsResult) data[prop] = blogsResult[prop]
+            return data;
+        })
+        .catch((error) => console.error(error));
+};
+
+module.exports.updateContact = function (req) {
+
+    return Contact
+    .findOne({where: {id: req.body.id}})
+    .then(blogResponse => {
+        if(blogResponse){
+            return Contact.update(req.body , { where: { id: req.body.id } }).then(response => {
+                return req.body
+            }).catch(error => {
+                console.log(error)
+                return error ;
+            });
+        }else{
+            return {"message":"Data Not found"}
+        }
+
+    })
+    .catch((error) => console.error(error));
+};
+
+module.exports.getContact = function (category) {
+    console.log("Data ",category )
+
+    return Contact
     .findAll({where: {category: category}})
     .then(blogResponse => {
         return {
--- routes/index.js
+++ routes/index.js
@@ -105,6 +105,69 @@ router.post('/create_blogs',upload.single('image'), router.oauth.authenticate(),
 });
 
 
+/**
+ * Contact Details 
+ */
+
+/**
+ * Creating the contact based on category 
+ * Join us 
+ * contact us
+ */
+ router.post('/create_contact', router.oauth.authenticate(), function (req, res) {
+  console.log(JSON.stringify(router.oauth.authenticate()))
+  let validation = Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    contact_no: Joi.string().required(),
+    category: Joi.string().required()
+  });
+  let result = validation.validate(req.body, { abortEarly: false });
+  if (result.error) {
+    let data = result.error.details[0].message.replace(
+      /[."*+\-?^${}()|[\]\\]/g,
+      ""
+    );
+    res.send({ code: 400, err: data });
+    return;
+  }
+
+  Functioncontroller.createContact(req.body).then(blogs =>res.json(blogs) ).catch(error=> { res.json((error)); }   )
+  //res.json("Secret");
+});
+
+/**
+ * update the contact based on category 
+ * Join us 
+ * contact us
+ */
+ router.put('/update_contact',upload.single('image'), router.oauth.authenticate(), function (req, res) {
+  console.log(JSON.stringify(router.oauth.authenticate()))
+ 
+  console.log(req.body.id)
+  Functioncontroller.updateContact(req).then(blogs =>{
+    res.status(200).json(blogs) 
+  }).catch(error=> { res.json((error)); }   )
+  //res.json("Secret");
+});
+
+/**
+ * Get List based on contact category 
+ * //etc. example.com/user/000000?sex=female
+ * query = {sex:"female"}
+ * params = {id:"000000"}
+ * 
+ */
+ router.get('/get_contact', router.oauth.authenticate(), function (req, res) {
+  console.log(JSON.stringify(router.oauth.authenticate()))
+ 
+  console.log("Req Data  : ",req.query.category)
+  Functioncontroller.getContact(req.query.category).then(blogs =>res.json(blogs) ).catch(error=> { res.json((error)); }   )
+
+  //res.json("Secret");
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Eugia' });
