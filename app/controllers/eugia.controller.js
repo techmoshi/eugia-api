0e6342e8955c27ec9ca7671da85c7282eac05912
Arif K K <arifkk0313@gmail.com>
2022-09-26 15:26:58 +0530

    designation field added director

--- app/controllers/eugia.controller.js
+++ app/controllers/eugia.controller.js
@@ -57,6 +57,7 @@ exports.create = (req, res) => {
               validation = Joi.object().keys({
                 name: Joi.string().required(),
                 designation: Joi.string().required(),
+                description: Joi.string().required(),
                 image: Joi.string().required(),
                 category: Joi.string().required()
               });
@@ -135,6 +136,7 @@ exports.create = (req, res) => {
                                 validation = Joi.object().keys({
                                   title: Joi.string().required(),
                                   description: Joi.string().required(),
+                                  designation:Joi.string().required(),
                                   image: Joi.string().required(),
                                   category: Joi.string().required(),
                                 });
